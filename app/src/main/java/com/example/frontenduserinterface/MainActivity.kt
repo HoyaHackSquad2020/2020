923b52b43c0118fe901afc4a04c1d7968a483684
jjs8 <jeffrey.schwartz@nist.gov>
2020-02-02 01:56:14 -0500

    Update a lot of things

--- app/src/main/java/com/example/frontenduserinterface/MainActivity.kt
+++ app/src/main/java/com/example/frontenduserinterface/MainActivity.kt
@@ -1,77 +1,84 @@
 package com.example.frontenduserinterface
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+import android.graphics.Color
 import android.os.Bundle
-import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.view.View
 import android.widget.EditText
-import java.util.Random
-import androidx.core.app.ComponentActivity
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-import java.util.regex.Pattern
+import androidx.appcompat.app.AppCompatActivity
+
+//import com.mongodb.MongoClient
+//import com.mongodb.MongoClientURI
+import com.mongodb.client.MongoDatabase
+
 
 
 class MainActivity : AppCompatActivity() {
+
+    //private lateinit var fusedLocationClient: FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
     }
 
-
-    /** Called when the user taps the Send button*/
+    // Called when user presses cmdSend
     fun sendMessage(view: View){
-        val newText = findViewById<EditText>(R.id.txtrecycle)
-        newText.setText("wrong");
 
-        val editText = findViewById<EditText>(R.id.txtzipcode)
-        val zip = editText.text.toString()
-        zip.trim()
-//        val myPattern = "\\d{5}";
-//        val validCharacterPattern = Pattern.compile(myPattern)
-//        val matcher = validCharacterPattern.matcher(zip.toString());
-//        val b = matcher.find();
+        //val uri = MongoClientURI("mongodb+srv://js:hoyahackathon@cluster0-yieuo.mongodb.net/test?retryWrites=true&w=majority")
+        //val mongoClient = MongoClient(uri)
+        //val database: MongoDatabase = mongoClient.getDatabase("zip_codes")
+        //val myCollection = database.getCollection("usa")
+        //val query = myCollection.find().limit(5)
+
+
+        val lblResults = findViewById<EditText>(R.id.lblResults)
+        lblResults.setTextColor(Color.RED)
+        lblResults.setText("Incorrect")
+        val txtZIPcode = findViewById<EditText>(R.id.txtZIPcode)
+        val zip = txtZIPcode.text.toString().trim() // Read ZIP code and trim whitespace
         if(zip.length == 5) {
-            val x = zip.toIntOrNull();
+            val x = zip.toIntOrNull()
             if (x != null) {
-
                 if ((x % 1) == 0) {
                     if (x > 0) {
-                        val actualText = findViewById<EditText>(R.id.txtrecycle)
-                        actualText.setText("correct");
-
+                        lblResults.setText("Correct")
+                        lblResults.setTextColor(Color.GREEN)
                     }
                 }
-                //        if (b) {
-                //                if ()
-                //Do something in response to button
             }
         }
-//        }
-//        else{
-//            val editText = findViewById<EditText>(R.id.txtrecycle)
-//            editText.setText("wrong");
-//        }
     }
 
+    // Called when user presses cmdZIPcode
     fun zipcode(view: View){
         val rand = Random();
+        val randString = StringBuilder()
+        for (x in 0 until 5) // Generates a 5-digit random number
+        {
+            randString.append((rand.nextInt(9) + 1).toString())
+        }
+        val txtZIPcode = findViewById<EditText>(R.id.txtZIPcode)
+        txtZIPcode.setText(randString.toString())
 
-        val n = rand.nextInt(99999) + 1;
-
-            //Do something in response to button
-            val editText = findViewById<EditText>(R.id.txtzipcode)
-            editText.setText(n.toString());
+        /*fusedLocationClient.lastLocation
+            .addOnSuccessListener { location : Location? ->
+                // Got last known location. In some rare situations this can be null.
+        }*/
     }
 
+    // Called when user presses cmdRecycleCode
     fun recyclecode(view: View){
-        val rand = Random();
-        val n = rand.nextInt(7) + 1;
-//        Do something in response to button
-        val editText = findViewById<EditText>(R.id.txtrecycle)
-//        editText.setText("123");
-        editText.setText(n.toString());
+        val rand = Random()
+        val n = rand.nextInt(7) + 1 // Generates a random number between 1 and 7
+        val txtRecycleCode = findViewById<EditText>(R.id.txtRecycleCpde)
+        txtRecycleCode.setText(n.toString())
     }
+
+
+
+
+
 }
